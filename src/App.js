3cb2c31253abad63b24dae94645f457c6768143d
Allan Li <allan0499@gmail.com>
2021-12-28 19:43:47 +0200

    Remove unnecessary useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Form from './Form';
 import Information from './Information';
@@ -6,16 +6,11 @@ import Information from './Information';
 const App = () => {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    console.log('use effect ran');
-    console.log(data);
-  }, [data]);
-
   return (
     <div className="App">
       <div className="content">
         <h1>Scrooge McCrypto</h1>
-        <Form analyze={(json) => setData(json)} />
+        <Form analyze={async (json) => await setData(json)} />
         {data && <Information data={data} />}
       </div>
     </div>
